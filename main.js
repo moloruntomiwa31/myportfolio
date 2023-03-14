9945c071ae2641474ddfa000a508f2282df94397
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-03-14 15:17:24 +0100

    Initial files uploaded

---
+++ main.js
@@ -0,0 +1,23 @@
+// let rView = document.getElementById("r-view");
+// let cView = document.getElementById("c-view");
+// let nView = document.getElementById("n-view");
+// let sView = document.getElementById("s-view");
+
+// let a1 = document.getElementById("a1");
+// let a2 = document.getElementById("a2");
+// let a3 = document.getElementById("a3");
+// let a4 = document.getElementById("a4");
+// let count = 0;
+// rView.innerText = count;
+// cView.innerText = count;
+// nView.innerText = count;
+// sView.innerText = count;
+// cView.innerText = count
+// const incre = (viewEl) => {
+//     count++;
+//     viewEl.innerText = count
+// }
+// a1.addEventListener("click", incre(rView))
+// a2.addEventListener("click", incre(cView))
+// a3.addEventListener("click", incre(nView))
+// a4.addEventListener("click", incre(sView))
