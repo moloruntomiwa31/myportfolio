eecb0e7db8eb84bf2955c2990993aac0124d40eb
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-05-01 04:23:08 +0100

    Reafactored code

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 const themeBtn = document.getElementById("btn");
-
+const border = document.querySelector(".container .row .img-fluid")
+let borderColor = "white"
 themeBtn.addEventListener("click", ()  => {
     const element = document.body
     element.classList.toggle("light-mode")
+    if (borderColor == "white") {
+        border.style.borderColor = "#0a2547"
+        borderColor = "#0a2547"
+    } else {
+        border.style.borderColor = "white"
+        borderColor = "white"
+    }
 }) 
