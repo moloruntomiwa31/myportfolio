94f3799f2863ba3ff769eda5603a909eb6e2af22
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-07-23 19:50:10 -0700

    added new features

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-const themeBtn = document.getElementById("btn");
-const border = document.querySelector(".container .row .img-fluid")
-const preLoader = document.querySelector(".preloader");
-let borderColor = "white"
-themeBtn.addEventListener("click", ()  => {
-    const element = document.body
-    element.classList.toggle("light-mode")
-    if (borderColor == "white") {
-        border.style.borderColor = "#0a2547"
-        borderColor = "#0a2547"
-    } else {
-        border.style.borderColor = "white"
-        borderColor = "white"
-    }
-}) 
+// const themeBtn = document.getElementById("btn");
+// const border = document.querySelector(".container .row .img-fluid")
+// const preLoader = document.querySelector(".preloader");
+// let borderColor = "white"
+// themeBtn.addEventListener("click", ()  => {
+//     const element = document.body
+//     element.classList.toggle("light-mode")
+//     if (borderColor == "white") {
+//         border.style.borderColor = "#0a2547"
+//         borderColor = "#0a2547"
+//     } else {
+//         border.style.borderColor = "white"
+//         borderColor = "white"
+//     }
+// }) 
 
 
-window.addEventListener("load", () => {
-    preLoader.style.display = "none"
-})
+// window.addEventListener("load", () => {
+//     preLoader.style.display = "none"
+// })
