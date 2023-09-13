ee456aba62fd770138b3fce8e1957d586530e1ec
moloruntomiwa31 <moloruntomiwa31@gmail.com>
2023-09-13 08:01:32 -0700

    .

--- js/main.js
+++ js/main.js
@@ -1,13 +1,20 @@
 const themeBtn = document.getElementById("btn");
 const preLoader = document.querySelector(".preloader");
 const content = document.getElementById("content");
-themeBtn.addEventListener("click", ()  => {
-    const element = document.body
-    element.classList.toggle("light-mode")
-}) 
+themeBtn.addEventListener("click", () => {
+  const element = document.body;
+  element.classList.toggle("light-mode");
+});
 
 window.onbeforeunload = () => {
-    for(const form of document.getElementsByTagName('form')) {
-      form.reset();
-    }
-}
+  for (const form of document.getElementsByTagName("form")) {
+    form.reset();
+  }
+};
+
+const connectForm = document.getElementById("connectForm");
+connectForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  connectForm.reset();
+  alert("Form submitted successfully!");
+});
